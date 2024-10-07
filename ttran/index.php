5d49525df837e913f9aceb5e32e9aa0b4d63810e
ttiff <90669090+ttiff@users.noreply.github.com>
2024-10-06 23:42:16 -0600

    Create index.php and apply initial styles using semantic UI and CSS

---
+++ ttran/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/index.css">
+    <!-- Stylesheet sourced from Semantic UI CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+
+</head>
+
+<body>
+    <header>
+        <div class="ui dark large secondary pointing menu">
+            <a class="active item">Home</a>
+            <a class="item" href="browse.php">Browse</a>
+            <a class="item" href="drivers.php">Drivers</a>
+            <a class="item" href="constructors.php">Constructors</a>
+            <a class="item" href="api-tester.php">APIs</a>
+            <a class="item" href="https://github.com/ttiff/comp3512-assignment1">GitHub</a>
+        </div>
+    </header>
+    <main class="background-section">
+        <div class="text-content">
+            <h1 class="ui header">COMP 3512 F1 Dashboard.</h1>
+            <p>Explore race results, driver performances, and more from the 2022 F1 season. </p>
+            <a href="browse.php" class="ui button primary">Browse 2022 Season</a>
+        </div>
+    </main>
+    <footer class="ui inverted vertical footer segment">
+        <div class="ui container">
+            <div class="ui section divider"></div>
+            <div class="ui center aligned container">
+                <p>&copy; MRU COMP 3512 Assignment #1 by Tiffany Tran. Built using HTML, CSS, PHP, MySQL, and Semantic UI.</p>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
