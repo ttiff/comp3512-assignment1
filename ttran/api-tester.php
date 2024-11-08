a3907d2c87144077cfa54ee74ffcd53404c6ffe7
ttiff <90669090+ttiff@users.noreply.github.com>
2024-11-08 13:21:34 -0700

    Update API links in api-tester.php to use relative paths (e.g., api/circuits.php) instead of absolute paths for compatibility across different environments.

--- ttran/api-tester.php
+++ ttran/api-tester.php
@@ -33,51 +33,51 @@
                 </thead>
                 <tbody>
                     <tr>
-                        <td><a href="/comp3512-assignment1/ttran/api/circuits.php" target="_blank">/api/circuits.php</a></td>
+                        <td><a href="api/circuits.php" target="_blank">/api/circuits.php</a></td>
                         <td>Returns all the circuits for the season</td>
                     </tr>
                     <tr>
-                        <td><a href="/comp3512-assignment1/ttran/api/circuits.php?ref=monaco" target="_blank">/api/circuits.php?ref=monaco</a></td>
+                        <td><a href="api/circuits.php?ref=monaco" target="_blank">/api/circuits.php?ref=monaco</a></td>
                         <td>Returns just the specified circuit (Monaco)</td>
                     </tr>
                     <tr>
-                        <td><a href="/comp3512-assignment1/ttran/api/constructors.php" target="_blank">/api/constructors.php</a></td>
+                        <td><a href="api/constructors.php" target="_blank">/api/constructors.php</a></td>
                         <td>Returns all the constructors for the season</td>
                     </tr>
                     <tr>
-                        <td><a href="/comp3512-assignment1/ttran/api/constructors.php?ref=mclaren" target="_blank">/api/constructors.php?ref=mclaren</a></td>
+                        <td><a href="api/constructors.php?ref=mclaren" target="_blank">/api/constructors.php?ref=mclaren</a></td>
                         <td>Returns just the specified constructor (Mclaren)</td>
                     </tr>
                     <tr>
-                        <td><a href="/comp3512-assignment1/ttran/api/drivers.php" target="_blank">/api/drivers.php</a></td>
+                        <td><a href="api/drivers.php" target="_blank">/api/drivers.php</a></td>
                         <td>Returns all the drivers for the season</td>
                     </tr>
                     <tr>
-                        <td><a href="/comp3512-assignment1/ttran/api/drivers.php?ref=hamilton" target="_blank">/api/drivers.php?ref=hamilton</a></td>
+                        <td><a href="api/drivers.php?ref=hamilton" target="_blank">/api/drivers.php?ref=hamilton</a></td>
                         <td>Returns just the specified driver (Hamilton)</td>
                     </tr>
                     <tr>
-                        <td><a href="/comp3512-assignment1/ttran/api/drivers.php?race=1106" target="_blank">/api/drivers.php?race=1106</a></td>
+                        <td><a href="api/drivers.php?race=1106" target="_blank">/api/drivers.php?race=1106</a></td>
                         <td>Returns the drivers within a given race (1106)</td>
                     </tr>
                     <tr>
-                        <td><a href="/comp3512-assignment1/ttran/api/races.php?ref=1106" target="_blank">/api/races.php?ref=1106</a></td>
+                        <td><a href="api/races.php?ref=1106" target="_blank">/api/races.php?ref=1106</a></td>
                         <td>Returns the specified race details (1106)</td>
                     </tr>
                     <tr>
-                        <td><a href="/comp3512-assignment1/ttran/api/races.php" target="_blank">/api/races.php</a></td>
+                        <td><a href="api/races.php" target="_blank">/api/races.php</a></td>
                         <td>Returns the races within the 2022 season ordered by round</td>
                     </tr>
                     <tr>
-                        <td><a href="/comp3512-assignment1/ttran/api/qualifying.php?ref=1106" target="_blank">/api/qualifying.php?ref=1106</a></td>
+                        <td><a href="api/qualifying.php?ref=1106" target="_blank">/api/qualifying.php?ref=1106</a></td>
                         <td>Returns the qualifying results for the specified race (1106)</td>
                     </tr>
                     <tr>
-                        <td><a href="/comp3512-assignment1/ttran/api/results.php?ref=1106" target="_blank">/api/results.php?ref=1106</a></td>
+                        <td><a href="api/results.php?ref=1106" target="_blank">/api/results.php?ref=1106</a></td>
                         <td>Returns the race results for the specified race (1106)</td>
                     </tr>
                     <tr>
-                        <td><a href="/comp3512-assignment1/ttran/api/results.php?driver=max_verstappen" target="_blank">/api/results.php?driver=max_verstappen</a></td>
+                        <td><a href="api/results.php?driver=max_verstappen" target="_blank">/api/results.php?driver=max_verstappen</a></td>
                         <td>Returns all the results for a given driver (max_verstappen)</td>
                     </tr>
                 </tbody>
